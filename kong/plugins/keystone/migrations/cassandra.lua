6d95124aae50cedec2556594090462d67f211f9c
lenaaxenova <lenaaxenova@mail.ru>
2017-07-04 17:34:26 +0300

    Update cassandra.lua

--- kong/plugins/keystone/migrations/cassandra.lua
+++ kong/plugins/keystone/migrations/cassandra.lua
@@ -1,20 +1,20 @@
-return 
+return {
   {
     name = "keystone",
     up = [[
-      CREATE TABLE IF NOT EXISTS user_info(
+      CREATE TABLE IF NOT EXISTS keystone_user_info(
         user_id uuid,
-	tenant_id uuid,
+	    tenant_id uuid,
         user_name text,
         password text,
-	email text,
+	    email text,
         enabled boolean,
         PRIMARY KEY (user_id)
       );
       
-      CREATE INDEX IF NOT EXISTS ON user_info(user_name);
+      CREATE INDEX IF NOT EXISTS ON keystone_user_info(user_name);
 
-      CREATE TABLE IF NOT EXISTS tenant_info(
+      CREATE TABLE IF NOT EXISTS keystone_tenant_info(
         tenant_id uuid,
         tenant_name text,
         description text,
@@ -22,43 +22,40 @@ return
         PRIMARY KEY (tenant_id)
       );
    
-      CREATE INDEX IF NOT EXISTS ON tenant_info(tenant_name);
+      CREATE INDEX IF NOT EXISTS ON keystone_tenant_info(tenant_name);
 
-      CREATE TABLE IF NOT EXISTS token_info(
-	token_id uuid,
+      CREATE TABLE IF NOT EXISTS keystone_token_info(
+	    token_id uuid,
         user_id uuid,
         tenant_id uuid,
         issued_at timestamp,
-	expires timestamp,
+	    expires timestamp,
         PRIMARY KEY (token_id)
       );
-
-      CREATE INDEX IF NOT EXISTS ON token_info(user_id);
       
-      CREATE TABLE IF NOT EXISTS uname_to_uid(
-	user_name text,
-	user_id uuid,
-	PRIMARY KEY (user_name)
+      CREATE TABLE IF NOT EXISTS keystone_uname_to_uid(
+	    user_name text,
+	    user_id uuid,
+	    PRIMARY KEY (user_name)
       );
 
-      CREATE INDEX IF NOT EXISTS ON uname_to_uid(user_id);
+      CREATE INDEX IF NOT EXISTS ON keystone_uname_to_uid(user_id);
     
-      CREATE TABLE IF NOT EXISTS tenname_to_tenid(
+      CREATE TABLE IF NOT EXISTS keystone_tenname_to_tenid(
         tenant_name text, 
         tenant_id uuid,
         PRIMARY KEY (tenant_name)
       );
 
-      CREATE INDEX IF NOT EXISTS ON tenname_to_tenid(tenant_id);
+      CREATE INDEX IF NOT EXISTS ON keystone_tenname_to_tenid(tenant_id);
 
     ]],
     down = [[
-      DROP TABLE uid_to_uinfo;
-      DROP TABLE tenid_to_teninfo;
-      DROP TABLE tokenid_to_tokeninfo;
-      DROP TABLE uname_to_uid;
-      DROP TABLE tenname_to_tenid;
+      DROP TABLE keystone_user_info;
+      DROP TABLE keystone_tenant_info;
+      DROP TABLE keystone_token_info;
+      DROP TABLE keystone_uname_to_uid;
+      DROP TABLE keystone_tenname_to_tenid;
     ]]
+  }
 }
-
-
