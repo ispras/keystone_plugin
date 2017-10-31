bb15ff57d1a09f234811cb6ceb5a9a1b25c772fa
lenaaxenova <lenaaxenova@mail.ru>
2017-10-31 19:52:21 +0300

    Tables titles fixed

--- kong/plugins/keystone/daos.lua
+++ kong/plugins/keystone/daos.lua
@@ -1,7 +1,7 @@
 
 local ACCESS_TOKEN_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_access_token",
+    table = "access_token",
     fields = {
         id = { type = "id", required = true },
         access_secret = { type = "string", required = true },
@@ -15,7 +15,7 @@ local ACCESS_TOKEN_SCHEMA = {
 
 local ASSIGNMENT_SCHEMA = {
     primary_key = {"type", "actor_id", "target_id", "role_id", "inherited"},
-    table = "keystone_assignment",
+    table = "assignment",
     fileds = {
         type = { type = "string", enum = {"UserProject", "GroupProject", "GroupDomain"}, required = true },
         actor_id = { type = "id", required = true },
@@ -27,7 +27,7 @@ local ASSIGNMENT_SCHEMA = {
 
 local CONFIG_REGISTER_SCHEMA = {
     primary_key = {"type"},
-    table = "keystone_config_register",
+    table = "config_register",
     fields = {
         type = { type = "string", required = true },
         domain_id = { type = "id", required = true }
@@ -36,7 +36,7 @@ local CONFIG_REGISTER_SCHEMA = {
 
 local CONSUMER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_consumer",
+    table = "consumer",
     fileds = {
         id = { type = "id", required = true },
         description = { type = "string" },
@@ -47,7 +47,7 @@ local CONSUMER_SCHEMA = {
 
 local CREDENTIAL_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_credential",
+    table = "credential",
     fileds = {
         id = { type = "id", required = true },
         user_id = { type = "id", required = true },
@@ -61,7 +61,7 @@ local CREDENTIAL_SCHEMA = {
 
 local ENDPOINT_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_endpoint",
+    table = "endpoint",
     fields = {
         id = { type = "id", required = true },
         legacy_endpoint_id = { type = "id" },
@@ -76,7 +76,7 @@ local ENDPOINT_SCHEMA = {
 
 local ENDPOINT_GROUP_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_endpoint_group",
+    table = "endpoint_group",
     fields = {
         id = { type = "id", required = true },
         name = { type = "string", required = true },
@@ -87,7 +87,7 @@ local ENDPOINT_GROUP_SCHEMA = {
 
 local FEDERATED_USER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_federated_user",
+    table = "federated_user",
     fields = {
         id = { type = "id", required = true },
         user_id = { type = "id", required = true, queryable = true },
@@ -100,7 +100,7 @@ local FEDERATED_USER_SCHEMA = {
 
 local FEDERATION_PROTOCOL_SCHEMA = {
     primary_key = {"id", "idp_id"},
-    table = "keystone_federation_protocol",
+    table = "federation_protocol",
     fields = {
         id = { type = "id", required = true },
         idp_id = { type = "id", required = true },
@@ -110,7 +110,7 @@ local FEDERATION_PROTOCOL_SCHEMA = {
 
 local GROUP_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_group",
+    table = "group",
     fileds = {
         id = { type = "id", required = true },
         domain_id = { type = "id", required = true, queryable = true },
@@ -122,7 +122,7 @@ local GROUP_SCHEMA = {
 
 local ID_MAPPING_SCHEMA = {
     primary_key = {"public_id"},
-    table = "keystone_id_mapping",
+    table = "id_mapping",
     fields = {
         public_id = { type = "id", required = true },
         domain_id = { type = "id", required = true, queryable = true },
@@ -133,7 +133,7 @@ local ID_MAPPING_SCHEMA = {
 
 local IDENTITY_PROVIDER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_identity_provider",
+    table = "identity_provider",
     fields = {
         id = { type = "id", required = true },
         enabled = { type = "boolean", required = true },
@@ -144,7 +144,7 @@ local IDENTITY_PROVIDER_SCHEMA = {
 
 local IDP_REMOTE_IDS_SCHEMA = {
     primary_key = {"remote_id"},
-    table = "keystone_idp_remote_ids",
+    table = "idp_remote_ids",
     fileds = {
         idp_id = { type = "id", queryable = true },
         remote_id = { type = "id", required = true }
@@ -153,7 +153,7 @@ local IDP_REMOTE_IDS_SCHEMA = {
 
 local IMPLIED_ROLE_SCHEMA = {
     primary_key = {"prior_role_id", "implied_role_id"},
-    table = "keystone_implied_role",
+    table = "implied_role",
     fileds = {
         prior_role_id = { type = "id", required = true },
         implied_role_id = { type = "id", required = true }
@@ -162,7 +162,7 @@ local IMPLIED_ROLE_SCHEMA = {
 
 local LOCAL_USER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_local_user",
+    table = "local_user",
     fileds = {
         id = { type = "id", required = true },
         user_id = { type = "id", required = true, unique = true, queryable = true },
@@ -175,7 +175,7 @@ local LOCAL_USER_SCHEMA = {
 
 local MAPPING_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_mapping",
+    table = "mapping",
     fields = {
         id = { type = "id", required = true },
         rules = { type = "string", required = true }
@@ -184,7 +184,7 @@ local MAPPING_SCHEMA = {
 
 local MIGRATE_VERSION_SCHEMA = {
     primary_key = {"repository_id"},
-    table = "keystone_migrate_version",
+    table = "migrate_version",
     fileds = {
         repository_id = { type = "id", required = true },
         repository_path = { type = "string" },
@@ -194,7 +194,7 @@ local MIGRATE_VERSION_SCHEMA = {
 
 local NONLOCAL_USER_SCHEMA = {
     primary_key = {"domain_id", "name"},
-    table = "keystone_nonlocal_user",
+    table = "nonlocal_user",
     fields = {
         domain_id = { type = "id", required = true },
         name = { type = "string", required = true },
@@ -204,7 +204,7 @@ local NONLOCAL_USER_SCHEMA = {
 
 local PASSWORD_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_password",
+    table = "password",
     fileds = {
         id = { type = "id", required = true },
         local_user_id = { type = "id", required = true, queryable = true },
@@ -220,7 +220,7 @@ local PASSWORD_SCHEMA = {
 
 local POLICY_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_policy",
+    table = "policy",
     fields = {
         id = { type = "id", required = true },
         type = { type = "string", required = true },
@@ -231,7 +231,7 @@ local POLICY_SCHEMA = {
 
 local POLICY_ASSOCIATION_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_policy_asssociation",
+    table = "policy_asssociation",
     fields = {
         id = { type = "id", required = true },
         policy_id = { type = "id", required = true },
@@ -243,7 +243,7 @@ local POLICY_ASSOCIATION_SCHEMA = {
 
 local PROJECT_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_project",
+    table = "project",
     fileds = {
         id = { type = "id", required = true },
         name = { type = "string", required = true },
@@ -258,7 +258,7 @@ local PROJECT_SCHEMA = {
 
 local PROJECT_ENDPOINT_SCHEMA = {
     primary_key = {"endpoint_id", "project_id"},
-    table = "keystone_project_endpoint",
+    table = "project_endpoint",
     fileds = {
         endpoint_id = { type = "id", required = true },
         project_id = { type = "id", required = true }
@@ -267,7 +267,7 @@ local PROJECT_ENDPOINT_SCHEMA = {
 
 local PROJECT_ENDPOINT_GROUP_SCHEMA = {
     primary_key = {"enpoint_group_id", "project_id"},
-    table = "keystone_project_endpoint_group",
+    table = "project_endpoint_group",
     fileds = {
         endpoint_group_id = { type = "id", required = true },
         project_id = { type = "id", required = true }
@@ -276,7 +276,7 @@ local PROJECT_ENDPOINT_GROUP_SCHEMA = {
 
 local PROJECT_TAG_SCHEMA = {
     primary_key = {"project_id", "name"},
-    table = "keystone_project_tag",
+    table = "project_tag",
     fileds = {
         project_id = { type = "id", required = true },
         name = { type = "string", required = true }
@@ -285,7 +285,7 @@ local PROJECT_TAG_SCHEMA = {
 
 local REGION_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_region",
+    table = "region",
     fields = {
         id = { type = "id", required = true },
         description = { type = "string", required = true },
@@ -296,7 +296,7 @@ local REGION_SCHEMA = {
 
 local REQUEST_TOKEN_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_request_token",
+    table = "request_token",
     fileds = {
         id = { type = "id", required = true },
         request_secret = { type = "string", required = true },
@@ -311,7 +311,7 @@ local REQUEST_TOKEN_SCHEMA = {
 
 local REVOCATION_EVENT_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_revocation_event",
+    table = "revocation_event",
     fileds = {
         id = { type = "id", required = true },
         domain_id = { type = "id" },
@@ -331,7 +331,7 @@ local REVOCATION_EVENT_SCHEMA = {
 
 local ROLE_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_role",
+    table = "role",
     fileds = {
         id = { type = "id", required = true },
         name = { type = "string", required = true },
@@ -342,7 +342,7 @@ local ROLE_SCHEMA = {
 
 local SENSITIVE_CONFIG_SCHEMA = {
     primary_key = {"domain_id", "group", "option"},
-    table = "keystone_sensetive_config",
+    table = "sensetive_config",
     fileds = {
         domain_id = { type = "id", required = true },
         group = { type = "id", required = true },
@@ -353,7 +353,7 @@ local SENSITIVE_CONFIG_SCHEMA = {
 
 local SERVICE_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_service",
+    table = "service",
     fields = {
         id = { type = "id", required = true },
         type = { type = "string" },
@@ -364,7 +364,7 @@ local SERVICE_SCHEMA = {
 
 local SERVICE_PROVIDER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_service_provider",
+    table = "service_provider",
     fields = {
         auth_url = { type = "url", required = true },
         id = { type = "id", required = true },
@@ -377,7 +377,7 @@ local SERVICE_PROVIDER_SCHEMA = {
 
 local TOKEN_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_token",
+    table = "token",
     fileds = {
         id = { type = "id", required = true },
         expires = { type = "timestamp" },
@@ -390,7 +390,7 @@ local TOKEN_SCHEMA = {
 
 local TRUST_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_trust",
+    table = "trust",
     fields = {
         id = { type = "id", required = true },
         trustor_user_id = { type = "id", required = true },
@@ -406,7 +406,7 @@ local TRUST_SCHEMA = {
 
 local TRUST_ROLE_SCHEMA = {
     primary_key = {"trust_id", "role_id"},
-    table = "keystone_trust_role",
+    table = "trust_role",
     fields = {
         trust_id = { type = "id", required = true },
         role_id = { type = "id", required = true }
@@ -415,7 +415,7 @@ local TRUST_ROLE_SCHEMA = {
 
 local USER_SCHEMA = {
     primary_key = {"id"},
-    table = "keystone_user",
+    table = "user",
     fileds = {
         id = { type = "id", required = true },
         extra = { type = "table" },
@@ -429,7 +429,7 @@ local USER_SCHEMA = {
 
 local USER_GROUP_MEMBERSHIP_SCHEMA = {
     primary_key = {"user_id", "group_id"},
-    table = "keystone_user_group_membership",
+    table = "user_group_membership",
     fileds = {
         user_id = { type = "id", required = true },
         group_id = { type = "id", required = true }
@@ -438,7 +438,7 @@ local USER_GROUP_MEMBERSHIP_SCHEMA = {
 
 local USER_OPTION_SCHEMA = {
     primary_key = {"user_id", "option_id"},
-    table = "keystone_user_option",
+    table = "user_option",
     fileds = {
         user_id = { type = "id", required = true },
         option_id = { type = "id", required = true },
@@ -448,7 +448,7 @@ local USER_OPTION_SCHEMA = {
 
 local WHITELISTED_CONFIG_SCHEMA = {
     primary_key = {"domain_id", "group", "option"},
-    table = "keystone_whitelisted_config",
+    table = "whitelisted_config",
     fileds = {
         domain_id = { type = "id", required = true },
         group = { type = "string", required = true },
@@ -458,43 +458,43 @@ local WHITELISTED_CONFIG_SCHEMA = {
 }
 
 return {
-    keystone_access_token = ACCESS_TOKEN_SCHEMA,
-    keystone_assignment = ASSIGNMENT_SCHEMA,
-    keystone_config_register = CONFIG_REGISTER_SCHEMA,
-    keystone_consumer = CONSUMER_SCHEMA,
-    keystone_credential = CREDENTIAL_SCHEMA,
-    keystone_endpoint = ENDPOINT_SCHEMA,
-    keystone_endpoint_group = ENDPOINT_GROUP_SCHEMA,
-    keystone_federated_user = FEDERATED_USER_SCHEMA,
-    keystone_federation_protocol = FEDERATION_PROTOCOL_SCHEMA,
-    keystone_group = GROUP_SCHEMA,
-    keystone_id_mapping = ID_MAPPING_SCHEMA,
-    keystone_identity_provider = IDENTITY_PROVIDER_SCHEMA,
-    keystone_idp_remote_ids = IDP_REMOTE_IDS_SCHEMA,
-    keystone_implied_role = IMPLIED_ROLE_SCHEMA,
-    keystone_local_user = LOCAL_USER_SCHEMA,
-    keystone_mapping = MAPPING_SCHEMA,
-    keystone_migrate_version = MIGRATE_VERSION_SCHEMA,
-    keystone_nonlocal_user = NONLOCAL_USER_SCHEMA,
-    keystone_password = PASSWORD_SCHEMA,
-    keystone_policy = POLICY_SCHEMA,
-    keystone_policy_asssociation = POLICY_ASSOCIATION_SCHEMA,
-    keystone_project = PROJECT_SCHEMA,
-    keystone_project_endpoint = PROJECT_ENDPOINT_SCHEMA,
-    keystone_project_endpoint_group = PROJECT_ENDPOINT_GROUP_SCHEMA,
-    keystone_project_tag = PROJECT_TAG_SCHEMA,
-    keystone_region = REGION_SCHEMA,
-    keystone_request_token = REQUEST_TOKEN_SCHEMA,
-    keystone_revocation_event = REVOCATION_EVENT_SCHEMA,
-    keystone_role = ROLE_SCHEMA,
-    keystone_sensetive_config = SENSITIVE_CONFIG_SCHEMA,
-    keystone_service = SERVICE_SCHEMA,
-    keystone_service_provider = SERVICE_PROVIDER_SCHEMA,
-    keystone_token = TOKEN_SCHEMA,
-    keystone_trust = TRUST_SCHEMA,
-    keystone_trust_role = TRUST_ROLE_SCHEMA,
-    keystone_user = USER_SCHEMA,
-    keystone_user_group_membership = USER_GROUP_MEMBERSHIP_SCHEMA,
-    keystone_user_option = USER_OPTION_SCHEMA,
-    keystone_whitelisted_config = WHITELISTED_CONFIG_SCHEMA
+    access_token = ACCESS_TOKEN_SCHEMA,
+    assignment = ASSIGNMENT_SCHEMA,
+    config_register = CONFIG_REGISTER_SCHEMA,
+    consumer = CONSUMER_SCHEMA,
+    credential = CREDENTIAL_SCHEMA,
+    endpoint = ENDPOINT_SCHEMA,
+    endpoint_group = ENDPOINT_GROUP_SCHEMA,
+    federated_user = FEDERATED_USER_SCHEMA,
+    federation_protocol = FEDERATION_PROTOCOL_SCHEMA,
+    group = GROUP_SCHEMA,
+    id_mapping = ID_MAPPING_SCHEMA,
+    identity_provider = IDENTITY_PROVIDER_SCHEMA,
+    idp_remote_ids = IDP_REMOTE_IDS_SCHEMA,
+    implied_role = IMPLIED_ROLE_SCHEMA,
+    local_user = LOCAL_USER_SCHEMA,
+    mapping = MAPPING_SCHEMA,
+    migrate_version = MIGRATE_VERSION_SCHEMA,
+    nonlocal_user = NONLOCAL_USER_SCHEMA,
+    password = PASSWORD_SCHEMA,
+    policy = POLICY_SCHEMA,
+    policy_asssociation = POLICY_ASSOCIATION_SCHEMA,
+    project = PROJECT_SCHEMA,
+    project_endpoint = PROJECT_ENDPOINT_SCHEMA,
+    project_endpoint_group = PROJECT_ENDPOINT_GROUP_SCHEMA,
+    project_tag = PROJECT_TAG_SCHEMA,
+    region = REGION_SCHEMA,
+    request_token = REQUEST_TOKEN_SCHEMA,
+    revocation_event = REVOCATION_EVENT_SCHEMA,
+    role = ROLE_SCHEMA,
+    sensetive_config = SENSITIVE_CONFIG_SCHEMA,
+    service = SERVICE_SCHEMA,
+    service_provider = SERVICE_PROVIDER_SCHEMA,
+    token = TOKEN_SCHEMA,
+    trust = TRUST_SCHEMA,
+    trust_role = TRUST_ROLE_SCHEMA,
+    user = USER_SCHEMA,
+    user_group_membership = USER_GROUP_MEMBERSHIP_SCHEMA,
+    user_option = USER_OPTION_SCHEMA,
+    whitelisted_config = WHITELISTED_CONFIG_SCHEMA
 }
