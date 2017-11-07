d57b2d02c38bb140cece2c4225860601f24f4dbe
bura2017 <valeriya.shvetcova@phystech.edu>
2017-11-07 13:18:34 +0300

    api view changed

--- kong/plugins/keystone/api.lua
+++ kong/plugins/keystone/api.lua
@@ -1,38 +1,7 @@
-local user = require("views/users")
+local views = {}
+local users = require("views/users")
+for k, v in pairs(users) do
+    views[k] = v
+end
 
-return {
-    ["/v3/users"] = {
-        GET = function(self, dao_factory)
-            user.list_users(self, dao_factory)
-        end,
-        POST = function(self, dao_factory)
-            user.create_user(self, dao_factory)
-        end
-    },
-    ["/v3/users/:user_id"] = {
-        GET = function(self, dao_factory)
-            user.get_user_info(self, dao_factory)
-        end,
-        PATCH = function(self, dao_factory)
-            user.update_user(self, dao_factory)
-        end,
-        DELETE = function(self, dao_factory)
-            user.delete_user(self, dao_factory)
-        end
-    },
-    ["/v3/users/:user_id/groups"] = {
-        GET = function(self, dao_factory)
-            user.list_user_groups(self, dao_factory)
-        end
-    },
-    ["/v3/users/:user_id/projects"] = {
-        GET = function(self, dao_factory)
-            user.list_user_projects(self, dao_factory)
-        end
-    },
-    ["/v3/users/:user_id/password"] = {
-        POST = function(self, dao_factory)
-            user.change_user_password(self, dao_factory)
-        end
-    }
-}
+return views
