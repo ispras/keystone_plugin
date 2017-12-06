d1ed67e687592c287175351b689ef51970986f55
bura2017 <valeriya.shvetcova@phystech.edu>
2017-12-06 15:59:53 +0300

    only format

--- kong/plugins/keystone/views/domain_configuration.lua
+++ kong/plugins/keystone/views/domain_configuration.lua
@@ -1,70 +1,112 @@
-DomainConfiguration = {}
+local responses = require "kong.tools.responses"
+local utils = require "kong.tools.utils"
+local sha512 = require('sha512')
+local kutils = require ("kong.plugins.keystone.utils")
 
-function get_default_configuration(self, dao_factory)
+local function get_default_configuration(self, dao_factory)
     return ''
 end
 
-
-function get_default_configuration_for_group(self, dao_factory)
+local function get_default_configuration_for_group(self, dao_factory)
     return ''
 end
 
-function get_default_option_for_group(self, dao_factory)
+local function get_default_option_for_group(self, dao_factory)
     return ''
 end
 
-function get_domain_group_option_configuration(self, dao_factory)
+local function get_domain_group_option_configuration(self, dao_factory)
     return ''
 end
 
-function update_domain_group_option_configuration(self, dao_factory)
+local function update_domain_group_option_configuration(self, dao_factory)
     return ''
 end
 
-function delete_domain_group_option_configuration(self, dao_factory)
+local function delete_domain_group_option_configuration(self, dao_factory)
     return ''
 end
 
-function get_domain_group_configuration(self, dao_factory)
+local function get_domain_group_configuration(self, dao_factory)
     return ''
 end
 
-function update_domain_group_configuration(self, dao_factory)
+local function update_domain_group_configuration(self, dao_factory)
     return ''
 end
 
-function delete_domain_group_configuration(self, dao_factory)
+local function delete_domain_group_configuration(self, dao_factory)
     return ''
 end
 
-function create_domain_configuration(self, dao_factory)
+local function create_domain_configuration(self, dao_factory)
     return ''
 end
 
-function get_domain_configuration(self, dao_factory)
+local function get_domain_configuration(self, dao_factory)
     return ''
 end
 
-function update_domain_configuration(self, dao_factory)
+local function update_domain_configuration(self, dao_factory)
     return ''
 end
 
-function delete_domain_configuration(self, dao_factory)
+local function delete_domain_configuration(self, dao_factory)
     return ''
 end
 
-DomainConfiguration.get_default_configuration = get_default_configuration
-DomainConfiguration.get_default_configuration_for_group = get_default_configuration_for_group
-DomainConfiguration.get_default_option_for_group = get_default_option_for_group
-DomainConfiguration.get_domain_group_option_configuration = get_domain_group_option_configuration
-DomainConfiguration.update_domain_group_option_configuration = get_domain_group_option_configuration
-DomainConfiguration.delete_domain_group_option_configuration = delete_domain_group_option_configuration
-DomainConfiguration.get_domain_group_configuration = get_domain_group_configuration
-DomainConfiguration.update_domain_group_configuration = update_domain_group_configuration
-DomainConfiguration.delete_domain_group_configuration = delete_domain_group_configuration
-DomainConfiguration.create_domain_configuration = create_domain_configuration
-DomainConfiguration.get_domain_configuration = get_domain_configuration
-DomainConfiguration.update_domain_configuration = update_domain_configuration
-DomainConfiguration.delete_domain_configuration = delete_domain_configuration
+local routes = {
+    ['/v3/domains/confid/default'] = {
+        GET = function(self, dao_factory)
+            get_default_configuration(self, dao_factory)
+        end
+    },
+    ['/v3/domains/config/:group_id/default'] = {
+        GET = function(self, dao_factory)
+            get_default_configuration_for_group(self, dao_factory)
+        end
+    },
+    ['/v3/domains/config/:group_id/:option_name/default'] = {
+        GET = function(self, dao_factory)
+            get_default_option_for_group(self, dao_factory)
+        end
+    },
+    ['/v3/domains/:domain_id/config/:group_id/:option_name'] = {
+        GET = function(self, dao_factory)
+            get_domain_group_option_configuration(self, dao_factory)
+        end,
+        PATCH = function(self, dao_factory)
+            update_domain_group_option_configuration(self, dao_factory)
+        end,
+        DELETE = function (self, dao_factory)
+            delete_domain_group_option_configuration(self, dao_factory)
+        end
+    },
+    ['/v3/domains/:domain_id/config/:group_id'] = {
+        GET = function(self, dao_factory)
+            get_domain_group_configuration(self, dao_factory)
+        end,
+        PATCH = function(self, dao_factory)
+            update_domain_group_configuration(self, dao_factory)
+        end,
+        DELETE = function (self, dao_factory)
+            delete_domain_group_configuration(self, dao_factory)
+        end
+    },
+    ['/v3/domains/:domain_id/config'] = {
+        PUT = function(self, dao_factory)
+            create_domain_configuration(self, dao_factory)
+        end,
+        GET = function(self, dao_factory)
+            get_domain_configuration(self, dao_factory)
+        end,
+        PATCH = function(self, dao_factory)
+            update_domain_configuration(self, dao_factory)
+        end,
+        DELETE = function (self, dao_factory)
+            delete_domain_configuration(self, dao_factory)
+        end
+    }
+}
 
-return DomainConfiguration
+return routes
