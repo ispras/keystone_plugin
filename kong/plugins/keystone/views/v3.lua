ff00743a5274fc402b4b344c4d55f2afe7ff3bbb
bura2017 <valeriya.shvetcova@phystech.edu>
2017-12-20 19:41:19 +0300

    added init kong request

--- kong/plugins/keystone/views/v3.lua
+++ kong/plugins/keystone/views/v3.lua
@@ -2,6 +2,9 @@ local responses = require "kong.tools.responses"
 local kutils = require ("kong.plugins.keystone.utils")
 local json = require('cjson')
 local SERVER_IP = '127.0.0.1'
+local projects = require ('kong.plugins.keystone.views.projects')
+local roles = require ('kong.plugins.keystone.views.roles')
+local users = require ('kong.plugins.keystone.views.users')
 
 local function v3()
     local body = {
@@ -23,15 +26,117 @@ local function v3()
 	return responses.send_HTTP_OK(body, kutils.headers())
 end
 
+local function init(self, dao_factory)
+    local resp = {
+        default_domain_id = '',
+        admin_domain_id = '',
+        admin_project_id = '',
+        default_role_id = '',
+        admin_role_id = '',
+        admin_user_id = '',
+    }
+
+    self.params.project = {
+        description = "Default domain",
+        enabled = true,
+        is_domain = true,
+        name = "Default"
+    }
+    projects.create(self, dao_factory)
+    local temp, err = dao_factory.project:find_all({name = "Default", is_domain = true})
+    kutils.assert_dao_error(err, "project find all")
+    resp.default_domain_id = temp[1].id
+
+    self.params.project = {
+        description = "Admin domain",
+        enabled = true,
+        is_domain = true,
+        name = "admin"
+    }
+    projects.create(self, dao_factory)
+    local temp, err = dao_factory.project:find_all({name = "admin", is_domain = true})
+    kutils.assert_dao_error(err, "project find all")
+    resp.admin_domain_id = temp[1].id
+
+    self.params.project = {
+        description = "Admin project",
+        domain_id = resp.admin_domain_id,
+        enabled = true,
+        is_domain = false,
+        name = "admin"
+    }
+    projects.create(self, dao_factory)
+    local temp, err = dao_factory.project:find_all({name = "admin", is_domain = false, domain_id = resp.admin_domain_id})
+    kutils.assert_dao_error(err, "project find all")
+    resp.admin_project_id = temp[1].id
+
+    self.params.role = {
+        name = "default"
+    }
+    roles.create(self, dao_factory)
+    local temp, err = dao_factory.role:find_all({name = "default"})
+    kutils.assert_dao_error(err, "role find all")
+    resp.default_role_id = temp[1].id
+
+    self.params.role = {
+        name = "admin"
+    }
+    roles.create(self, dao_factory)
+    local temp, err = dao_factory.role:find_all({name = "admin"})
+    kutils.assert_dao_error(err, "role find all")
+    resp.admin_role_id = temp[1].id
+
+    local file = io.open('/etc/kong/admin_creds', "r")
+    if not file then
+        responses.send_HTTP_BAD_REQUEST("Failed to open creds file")
+    end
+    local name = file:read()
+    local password = file:read()
+    file:close()
+    if not name or not password then
+        responses.send_HTTP_BAD_REQUEST("Failed to read creds")
+    end
+
+    self.params.user = {
+        default_project_id = resp.admin_project_id,
+        domain_id = resp.admin_domain_id,
+        enabled = true,
+        name = name,
+        password = password
+    }
+    users.create(self, dao_factory)
+    local temp, err = dao_factory.local_user:find_all({name = name, domain_id = resp.admin_domain_id})
+    kutils.assert_dao_error(err, "local_user find all")
+    resp.admin_user_id = temp[1].user_id
+
+    self.params = {
+        actor_id = resp.admin_user_id,
+        target_id = resp.admin_project_id,
+        role_id = resp.admin_role_id
+    }
+    roles.assignment.assign(self, dao_factory, "UserProject", false, true)
+    self.params = {
+        actor_id = resp.admin_user_id,
+        target_id = resp.admin_domain_id,
+        role_id = resp.admin_role_id
+    }
+    roles.assignment.assign(self, dao_factory, "UserDomain", false, true)
+
+    return resp
+end
+
 return {
     ["/v3"] = {
         GET = function(self, dao_factory)
             v3()
+        end,
+        POST = function(self, dao_factory)
+            responses.send_HTTP_OK(init(self, dao_factory))
         end
     },
     ["/"] = {
         GET = function(self, dao_factory)
-            body = json.decode('{ "versions":{ "values":[ { "status":"stable", "updated":"2015-03-30T00:00:00Z", "media-types":[ { "base":"application/json", "type":"application/vnd.openstack.identity-v3+json" } ], "id":"v3.4", "links":[ { "href":"https://localhost:8001/v3/", "rel":"self" } ] } ] } }')
+            local body = json.decode('{ "versions":{ "values":[ { "status":"stable", "updated":"2015-03-30T00:00:00Z", "media-types":[ { "base":"application/json", "type":"application/vnd.openstack.identity-v3+json" } ], "id":"v3.4", "links":[ { "href":"https://localhost:8001/v3/", "rel":"self" } ] } ] } }')
             return responses.send_HTTP_OK(body, kutils.headers())
         end
     }
--- testing/base.py
+++ testing/base.py
@@ -29,122 +29,16 @@ class TestKeystoneBase(unittest.TestCase):
 
 
     def init(self):
-        body = {
-            "domain": {
-                "name": "Default",
-                "description": "Default domain for testing",
-                "enabled":  True
-            }
-        }
-        self.res = requests.post(self.host + '/v3/domains', json = body)
+        self.res = requests.post(self.host + '/v3', json = {})
+        self.checkCode(200)
 
-        # if self.res.text != "{\"message\":\"Error: project with this name exists\"}\n":
-        self.checkCode(201)
-        self.default_domain_id = self.res.json()['domain']['id']
-        #
-        # # body = {
-        # # "project": {
-        # #     "description": "Admin project",
-        # #     "enabled": True,
-        # #     "name": "Admin",
-        # #     }
-        # # }
-        # # self.res = requests.post(self.host + '/v3/projects', json = body)
-        # # if self.res.text != "{\"message\":\"Error: project with this name exists\"}\n":
-        # #     self.checkCode(201)
-        # #     self.project_id = self.res.json()['project']['id']
-        # #
-        # # self.checkCode(201)
-        # # self.default_domain_id = self.res.json()['domain']['id']
-        #
-        body = {
-            "domain": {
-                "name": "admin",
-                "description": "Admin domain for testing",
-                "enabled": True
-            }
-        }
-
-        # if self.res.text != "{\"message\":\"Error: project with this name exists\"}\n":
-        self.res = requests.post(self.host + '/v3/domains', json=body)
-        self.checkCode(201)
-        self.admin_domain_id = self.res.json()['domain']['id']
-
-
-        body = {
-            "project": {
-                "name": "admin",
-                "description": "Admin project for testing",
-                "enabled": True,
-                "is_domain": False,
-                "domain_id": self.admin_domain_id
-            }
-        }
-        self.res = requests.post(self.host + '/v3/projects', json=body)
-        self.checkCode(201)
-        self.admin_project_id = self.res.json()['project']['id']
-
-
-        body = {
-            'role' : {
-                'name' : 'Default'
-            }
-        }
-        self.res = requests.post(self.host + '/v3/roles', json = body)
-
-        if self.res.text != "{\"message\":\"Role with specified name already exists in domain\"}\n":
-            self.checkCode(201)
-            self.default_role_id = self.res.json()['role']['id']
-
-        # # body = {
-        # #     'role': {
-        # #         'name': 'Admin'
-        # #     }
-        # # }
-        # # self.res = requests.post(self.host + '/v3/roles', json=body)
-        # # if self.res.text != "{\"message\":\"Role with specified name already exists in domain\"}\n":
-        # #     self.checkCode(201)
-        # #     self.role_id = self.res.json()['role']['id']
-        #
-        # # self.checkCode(201)
-        # # self.default_role_id = self.res.json()['role']['id']
-        #
-        body = {
-            'role': {
-                'name': 'admin'
-            }
-        }
-        self.res = requests.post(self.host + '/v3/roles', json=body)
-        self.checkCode(201)
-        self.admin_role_id = self.res.json()['role']['id']
-
-        body = {
-            "user": {
-                "enabled": "true",
-                "name": "admin",
-                "password": "myadminpass",
-                "domain_id": self.admin_domain_id,
-                "default_project_id": self.admin_project_id
-
-            }
-        }
-        self.res = requests.post(self.host + '/v3/users', json = body)
-        #
-        # # if self.res.text != "{\"message\":\"Local user with this name is already exists\"}\n":
-        # #     self.checkCode(201)
-        # #     self.user_id = self.res.json()['user']['id']
-        #
-        self.checkCode(201)
-        self.admin_user_id = self.res.json()['user']['id']
-
-        self.res = requests.put(self.host + '/v3/domains/' + self.admin_domain_id + '/users/'
-                                + self.admin_user_id + '/roles/' + self.admin_role_id, json={})  # json object is required
-        self.checkCode(204)
-
-        self.res = requests.put(
-            self.host + '/v3/projects/' + self.admin_project_id + '/users/' + self.admin_user_id + '/roles/' + self.admin_role_id,
-            json={})  # json object is required
-        self.checkCode(204)
+        ids = self.res.json()
+        self.default_domain_id = ids['default_domain_id']
+        self.admin_domain_id = ids['admin_domain_id']
+        self.admin_project_id = ids['admin_project_id']
+        self.default_role_id = ids['default_role_id']
+        self.admin_role_id = ids['admin_role_id']
+        self.admin_user_id = ids['admin_user_id']
 
         body = {
             "region": {
