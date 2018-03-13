960cded12a2fc6b13757e18f4eeb98d84df2bafe
lenaaxenova <lenaaxenova@mail.ru>
2018-03-13 13:24:19 +0300

    create_ep_to_project_association doesn't work

--- kong/plugins/keystone/utils.lua
+++ kong/plugins/keystone/utils.lua
@@ -141,6 +141,43 @@ local function parse_header(header)
     return ret
 end
 
+local table_tostring
+local function table_val_to_str ( v )
+  if "string" == type( v ) then
+    v = string.gsub( v, "\n", "\\n" )
+    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+      return "'" .. v .. "'"
+    end
+    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+  else
+    return "table" == type( v ) and table_tostring( v ) or
+      tostring( v )
+  end
+end
+
+local function table_key_to_str ( k )
+  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+    return k
+  else
+    return "[" .. table_val_to_str( k ) .. "]"
+  end
+end
+
+table_tostring = function ( tbl )
+  local result, done = {}, {}
+  for k, v in ipairs( tbl ) do
+    table.insert( result, table.val_to_str( v ) )
+    done[ k ] = true
+  end
+  for k, v in pairs( tbl ) do
+    if not done[ k ] then
+      table.insert( result,
+        table_key_to_str( k ) .. "=" .. table_val_to_str( v ) )
+    end
+  end
+  return "{" .. table.concat( result, "," ) .. "}"
+end
+
 return {
     bool = bool,
     default_domain = default_domain,
@@ -153,5 +190,6 @@ return {
     subtree = subtree,
     config_from_dao = config_from_dao,
     provider = provider,
-    federated_group = federated_group
+    federated_group = federated_group,
+    table_to_string = table_tostring
 }
