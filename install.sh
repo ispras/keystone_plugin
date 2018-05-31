4678c42ded40d529434f1f530b80b3e97b420fc8
bura2017 <valeriya.shvetcova@phystech.edu>
2018-05-31 17:55:37 +0300

    small fixes

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo ">>> Configuration"
 #lua update_keystone_conf.lua
 curl -i -X POST   --url http://localhost:8001/apis/   --data 'name=mockbin'   --data 'upstream_url=http://mockbin.org/request'   --data 'uris=/'
 curl -i -X POST   --url http://localhost:8001/apis/mockbin/plugins/   --data 'name=keystone'
+curl -i -X POST   --url http://localhost:8001/v3/
 echo ">>> Save admin_creds in /etc/kong/admin_creps"
 sudo echo "$1
 $2" > /etc/kong/admin_creds
